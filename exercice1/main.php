c9b48f1ab5bb80bf62cfb707a0267219cec44f1f
samy <samybenchaabane@gmail.com>
2020-04-14 14:49:22 +0200

    add exercice

---
+++ exercice1/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Exo01</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>
+  </head>
+
+  <body>
+  </body>
+</html>
